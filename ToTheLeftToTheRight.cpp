14671878af54228c5ede2034234e5bdf430cf56b
Lordmau5 <mail@lordmau5.com>
2019-08-01 16:15:17 +0200

    Add 10% chance for To The Left To The Right to be twice as strong

--- ToTheLeftToTheRight.cpp
+++ ToTheLeftToTheRight.cpp
@@ -14,9 +14,14 @@ void ToTheLeftToTheRight::HandleTick() {
 			CCarCtrl::SwitchVehicleToRealPhysics(vehicle);
 		}
 
-		vehicle->m_vecMoveSpeed.x = RandomHelper::Random(-0.25f, 0.25f, 2);
-		vehicle->m_vecMoveSpeed.y = RandomHelper::Random(-0.25f, 0.25f, 2);
-		vehicle->m_vecMoveSpeed.z = RandomHelper::Random(-0.05f, 0.05f, 2);
+		int amplify = 2;
+		if (RandomHelper::Random(0, 9) == 0) {
+			amplify = 4;
+		}
+
+		vehicle->m_vecMoveSpeed.x = RandomHelper::Random(-0.25f, 0.25f, amplify);
+		vehicle->m_vecMoveSpeed.y = RandomHelper::Random(-0.25f, 0.25f, amplify);
+		vehicle->m_vecMoveSpeed.z = RandomHelper::Random(-0.05f, 0.05f, amplify);
 	}
 
 	wait = Random(2000, 5000);
