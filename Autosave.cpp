66431ce78714385bb05f8a413eaa1d1cf7af803f
Lordmau5 <mail@lordmau5.com>
2019-08-04 15:20:34 +0200

    Refactor Autosave
    Telling the user to get on foot or it won't save, also not advancing time by 6h anymore

--- Autosave.cpp
+++ Autosave.cpp
@@ -1,18 +1,18 @@
 #include "Autosave.h"
 
-Autosave::Autosave() : TimedEffect(1000 * 10, "Autosave") {}
+Autosave::Autosave() : TimedEffect(1000 * 10, "Autosave (Get On Foot)") {}
 
 void Autosave::HandleTick() {
 	if (!didSave && !FindPlayerVehicle(-1, false) && FindPlayerPed()) {
 		//std::sprintf(CGenericGameStorage::ms_ValidSaveName, "%s\\chaos_autosave.b", gamePath);
 		std::sprintf(CGenericGameStorage::ms_ValidSaveName, "%s\\GTASAsf8.b", gamePath); // Slot 8 until custom loading works
 
-		CGenericGameStorage::DoGameSpecificStuffBeforeSave();
-		bool saved = CGenericGameStorage::GenericSave(0);
+		CGenericGameStorage::GenericSave(0);
 
 		didSave = true;
 		
 		Disable();
-		remaining = 0;
+
+		description = "Autosave Completed";
 	}
 }
