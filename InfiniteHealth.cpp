39122d840eaaff1c4be86336a0f7a6535d61456d
Lordmau5 <mail@lordmau5.com>
2019-08-14 23:42:23 +0800

    Add fix-preparation for InfiniteHealth
    Pseudo-code to prevent peds from dying in cars

--- InfiniteHealth.cpp
+++ InfiniteHealth.cpp
@@ -8,6 +8,12 @@ InfiniteHealth::InfiniteHealth(int _duration, std::string _description)
 void InfiniteHealth::InitializeHooks() {
 	patch::RedirectCall(0x4B5B19, HookedAccountForPedArmour);
 	patch::RedirectCall(0x4B5B27, HookedComputeWillKillPed);
+
+	for (int address : {0x6B3950, 0x6B3D6C, 0x6BEAA3, 0x6C6F44, 0x6CCF83, 0x6F225A}) {
+		patch::RedirectCall(address, HookedKillPedsInVehicle);
+	}
+
+	patch::RedirectCall(0x6B3957, HookedKillPedsGettingInVehicle);
 }
 
 void InfiniteHealth::Enable() {
@@ -30,4 +36,22 @@ void __fastcall InfiniteHealth::HookedComputeWillKillPed(CPedDamageResponseCalcu
 	}
 
 	thisCalc->ComputeWillKillPed(ped, a3, a4);
+}
+
+void __fastcall InfiniteHealth::HookedKillPedsInVehicle(CVehicle* thisVehicle, void* edx) {
+	// TODO: Can't exit vehicle if vehicle blew up and player isn't dead
+	//if (isEnabled) {
+	//	return;
+	//}
+
+	thisVehicle->KillPedsInVehicle();
+}
+
+void __fastcall InfiniteHealth::HookedKillPedsGettingInVehicle(CVehicle* thisVehicle, void* edx) {
+	// TODO: Can't exit vehicle if vehicle blew up and player isn't dead
+	//if (isEnabled) {
+	//	return;
+	//}
+
+	thisVehicle->KillPedsGettingInVehicle();
 }
--- InfiniteHealth.h
+++ InfiniteHealth.h
@@ -20,4 +20,7 @@ public:
 private:
 	static void __fastcall HookedAccountForPedArmour(CPedDamageResponseCalculator* thisCalc, void* edx, CPed* ped, int cDamageResponseInfo);
 	static void __fastcall HookedComputeWillKillPed(CPedDamageResponseCalculator* thisCalc, void* edx, CPed* ped, float* a3, char a4);
+
+	static void __fastcall HookedKillPedsInVehicle(CVehicle* thisVehicle, void* edx);
+	static void __fastcall HookedKillPedsGettingInVehicle(CVehicle* thisVehicle, void* edx);
 };
