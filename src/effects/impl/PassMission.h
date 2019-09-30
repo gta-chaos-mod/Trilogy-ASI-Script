19c461522460012db7c0a2976a3cf9c9282aa68d
Lordmau5 <mail@lordmau5.com>
2019-09-30 02:42:47 +0200

    Fix: Implement Pass Current Mission mostly from scratch
    This is so we are able to support missions that didn't have it before or outright were not working properly

--- src/effects/impl/PassMission.h
+++ src/effects/impl/PassMission.h
@@ -1,15 +1,26 @@
 // Copyright (c) 2019 Lordmau5
 #pragma once
 
+#include <map>
+
 #include "effects/abstract/EffectPlaceholder.h"
+
+#include "effects/other/Player.h"
+#include "effects/other/Teleportation.h"
+
+#include "CCutsceneMgr.h"
+#include "extensions/ScriptCommands.h"
 
 class PassMission : public EffectPlaceholder
 {
 private:
-	int wait = 100;
-
-public:
 	static bool isEnabled;
+
+private:
+	std::map<std::string, int> offsetMap;
+	std::map<std::string, bool> debugCodeMap;
+	bool triedPassingMission = false;
+	int missionPassWait = 100;
 
 public:
 	PassMission(int _duration, const std::string& _description);
@@ -20,6 +31,8 @@ public:
 	void Disable() override;
 
 	void HandleTick() override;
+
+	void ClearPlayerStatus();
 
 	static char __stdcall HookedDebugGetKeyDown(int keyCode, int a2, int a3);
 };
