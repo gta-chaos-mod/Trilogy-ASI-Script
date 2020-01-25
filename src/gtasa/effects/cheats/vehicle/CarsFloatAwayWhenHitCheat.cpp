d8c8dad7543f2cc446b4055957d409e42aae130f
Lordmau5 <mail@lordmau5.com>
2020-01-25 11:57:56 +0100

    SA/Fix: Custom "Cars Float Away When Hit" implementation

--- src/gtasa/effects/cheats/vehicle/CarsFloatAwayWhenHitCheat.cpp
+++ src/gtasa/effects/cheats/vehicle/CarsFloatAwayWhenHitCheat.cpp
@@ -1,4 +1,49 @@
 #include "CarsFloatAwayWhenHitCheat.h"
 
+static ThiscallEvent<
+	AddressList<
+	0x54BDB2, H_CALL,
+	0x54BF78, H_CALL,
+	0x54C23A, H_CALL,
+	0x54C435, H_CALL,
+	0x54D17E, H_CALL,
+	0x54D27E, H_CALL,
+	0x54D3FE, H_CALL,
+	0x54D4D2, H_CALL
+	>,
+	PRIORITY_AFTER,
+	ArgPick<ArgTypes<CVehicle*, CVehicle*, int, float*, int>, 0, 1, 2, 3, 4>,
+	void(CVehicle*, CVehicle*, int, float*, int)>
+	applyCollisionEvent;
+
 CarsFloatAwayWhenHitCheat::CarsFloatAwayWhenHitCheat()
-	: TimedAddressEffect("cheat_cars_float_away_when_hit", 0x969166) {}
+	: EffectBase("cheat_cars_float_away_when_hit") {}
+
+void CarsFloatAwayWhenHitCheat::Enable() {
+	EffectBase::Enable();
+
+	applyCollisionEvent += ApplyCollision;
+}
+
+void CarsFloatAwayWhenHitCheat::Disable() {
+	applyCollisionEvent -= ApplyCollision;
+
+	CVehicle* playerVehicle = FindPlayerVehicle(-1, false);
+	if (playerVehicle) {
+		playerVehicle->m_nPhysicalFlags.bApplyGravity = true;
+	}
+
+	EffectBase::Disable();
+}
+
+void CarsFloatAwayWhenHitCheat::ApplyCollision(CVehicle* thisVehicle, CVehicle* a2, int a3, float* a4, int a5) {
+	CVehicle* playerVehicle = FindPlayerVehicle(-1, false);
+	if (!playerVehicle) return;
+
+	if (playerVehicle == thisVehicle) {
+		a2->m_nPhysicalFlags.bApplyGravity = false;
+	}
+	if (playerVehicle == a2) {
+		thisVehicle->m_nPhysicalFlags.bApplyGravity = false;
+	}
+}
--- src/gtasa/effects/cheats/vehicle/CarsFloatAwayWhenHitCheat.h
+++ src/gtasa/effects/cheats/vehicle/CarsFloatAwayWhenHitCheat.h
@@ -1,9 +1,14 @@
 #pragma once
 
-#include "effects/TimedAddressEffect.h"
+#include "util/EffectBase.h"
 
-class CarsFloatAwayWhenHitCheat : public TimedAddressEffect
+class CarsFloatAwayWhenHitCheat : public EffectBase
 {
 public:
 	CarsFloatAwayWhenHitCheat();
+
+	void Enable() override;
+	void Disable() override;
+
+	static void ApplyCollision(CVehicle* thisVehicle, CVehicle* a2, int a3, float* a4, int a5);
 };
