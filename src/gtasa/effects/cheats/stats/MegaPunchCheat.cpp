e39f3952904305c3a4518012c5726e77dd73de90
Lordmau5 <mail@lordmau5.com>
2020-01-25 11:54:39 +0100

    SA/Add: Mega Punch now allowing you to punch vehicles away

--- src/gtasa/effects/cheats/stats/MegaPunchCheat.cpp
+++ src/gtasa/effects/cheats/stats/MegaPunchCheat.cpp
@@ -2,3 +2,22 @@
 
 MegaPunchCheat::MegaPunchCheat()
 	: TimedAddressEffect("cheat_mega_punch", 0x969173) {}
+
+void MegaPunchCheat::InitializeHooks() {
+	TimedAddressEffect::InitializeHooks();
+
+	for (int address : { 0x624577, 0x66412C }) {
+		HookCall(address, HookedFightHitCar);
+	}
+}
+
+void __fastcall MegaPunchCheat::HookedFightHitCar(CTaskSimpleFight* thisTask, void* edx, CPed* ped, CVehicle* vehicle, CVector* colCoord, CVector* colDir, int colComp, int a7) {
+	float angle = ped->GetHeading();
+
+	CCarCtrl::SwitchVehicleToRealPhysics(vehicle);
+	vehicle->m_vecMoveSpeed.x = 2.5f * -sin(angle);
+	vehicle->m_vecMoveSpeed.y = 2.5f * cos(angle);
+	vehicle->m_vecMoveSpeed.z = 0;
+
+	CallMethod<0x61D0B0, CTaskSimpleFight*>(thisTask, ped, vehicle, colCoord, colDir, colComp, a7);
+}
--- src/gtasa/effects/cheats/stats/MegaPunchCheat.h
+++ src/gtasa/effects/cheats/stats/MegaPunchCheat.h
@@ -2,8 +2,14 @@
 
 #include "effects/TimedAddressEffect.h"
 
+#include "CCarCtrl.h"
+
 class MegaPunchCheat : public TimedAddressEffect
 {
 public:
 	MegaPunchCheat();
+
+	void InitializeHooks() override;
+
+	static void __fastcall HookedFightHitCar(CTaskSimpleFight* thisTask, void* edx, CPed* ped, CVehicle* vehicle, CVector* colCoord, CVector* colDir, int colComp, int a7);
 };
