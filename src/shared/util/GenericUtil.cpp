0b17888821716dce1cc7af2d0b370375a53c4f52
Lordmau5 <mail@lordmau5.com>
2020-01-27 18:51:39 +0100

    v2.1.1

--- src/shared/util/GenericUtil.cpp
+++ src/shared/util/GenericUtil.cpp
@@ -1,7 +1,7 @@
 #include "GenericUtil.h"
 
 std::string GenericUtil::GetModVersion() {
-	return "Chaos Mod v2.1.0";
+	return "Chaos Mod v2.1.1";
 }
 
 float GenericUtil::CalculateTick(float multiplier) {
