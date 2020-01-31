8c3bae2312d72ef4b02b849c9d4599edfa280871
Lordmau5 <mail@lordmau5.com>
2020-01-31 21:57:18 +0100

    SA/Fix: Change how `Let's Take A Break` works
    Disables movement at the cost of allowing e.g. phone calls - however, means controller support is in

--- src/gtasa/effects/custom/controls/LetsTakeABreakEffect.cpp
+++ src/gtasa/effects/custom/controls/LetsTakeABreakEffect.cpp
@@ -7,25 +7,10 @@ LetsTakeABreakEffect::LetsTakeABreakEffect()
 	SetEffectDuration(1000 * 10);
 }
 
-void LetsTakeABreakEffect::InitializeHooks() {
-	EffectBase::InitializeHooks();
-
-	HookCall(0x57C676, HookedOpenFile);
-
-	HookCall(0x577244, HookedCMenuManagerProcessPCMenuOptions);
-}
-
-void LetsTakeABreakEffect::Enable() {
-	EffectBase::Enable();
-
-	for (int i = 0; i < 59; i++) {
-		origActions[i] = ControlsManager.m_actions[i];
-	}
-}
-
 void LetsTakeABreakEffect::Disable() {
-	for (int i = 0; i < 59; i++) {
-		ControlsManager.m_actions[i] = origActions[i];
+	CPlayerPed* player = FindPlayerPed();
+	if (player && player->GetPadFromPlayer()) {
+		player->GetPadFromPlayer()->DisablePlayerControls = false;
 	}
 
 	EffectBase::Disable();
@@ -34,32 +19,8 @@ void LetsTakeABreakEffect::Disable() {
 void LetsTakeABreakEffect::HandleTick() {
 	EffectBase::HandleTick();
 
-	effectRemaining -= CalculateTick();
-	if (effectRemaining <= 0) {
-		Disable();
-		return;
+	CPlayerPed* player = FindPlayerPed();
+	if (player && player->GetPadFromPlayer()) {
+		player->GetPadFromPlayer()->DisablePlayerControls = true;
 	}
-
-	if (wait > 0) {
-		wait -= CalculateTick();
-		return;
-	}
-
-	for (int i = 0; i < 59; i++) {
-		ControlsManager.m_actions[i] = CControllerAction();
-	}
-
-	wait = 1000;
-}
-
-FILESTREAM LetsTakeABreakEffect::HookedOpenFile(const char* file, const char* mode) {
-	return 0;
-}
-
-void __fastcall LetsTakeABreakEffect::HookedCMenuManagerProcessPCMenuOptions(CMenuManager* thisManager, void* edx, eMenuPage page) {
-	if (page == eMenuPage::MENUPAGE_REDEFINE_CONTROLS) {
-		return;
-	}
-
-	GameFixes::HookedProcessMenuOptions(thisManager, edx, page);
 }
--- src/gtasa/effects/custom/controls/LetsTakeABreakEffect.h
+++ src/gtasa/effects/custom/controls/LetsTakeABreakEffect.h
@@ -1,29 +1,15 @@
 #pragma once
 
 #include "util/EffectBase.h"
-#include "util/GameFixes.h"
 
-#include "CControllerConfigManager.h"
-#include "CFileMgr.h"
+#include "extensions/ScriptCommands.h"
 
 class LetsTakeABreakEffect : public EffectBase
 {
-private:
-	int wait = 0;
-	int effectDuration = 1000 * 10;
-	int effectRemaining = 1000 * 10;
-	CControllerAction origActions[59] = {};
-
 public:
 	LetsTakeABreakEffect();
 
-	void InitializeHooks() override;
-
-	void Enable() override;
 	void Disable() override;
 
 	void HandleTick() override;
-
-	static FILESTREAM HookedOpenFile(const char* file, const char* mode);
-	static void __fastcall HookedCMenuManagerProcessPCMenuOptions(CMenuManager* thisManager, void* edx, eMenuPage page);
 };
