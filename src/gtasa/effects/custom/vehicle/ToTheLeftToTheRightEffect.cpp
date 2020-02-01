83d44e86cedf8065d52881341b97576b8382f725
Lordmau5 <mail@lordmau5.com>
2020-02-01 15:01:42 +0100

    SA/Fix: Change up `To The Left, To The Right`

--- src/gtasa/effects/custom/vehicle/ToTheLeftToTheRightEffect.cpp
+++ src/gtasa/effects/custom/vehicle/ToTheLeftToTheRightEffect.cpp
@@ -4,12 +4,14 @@ ToTheLeftToTheRightEffect::ToTheLeftToTheRightEffect()
 	: EffectBase("effect_to_the_left_to_the_right") {}
 
 void ToTheLeftToTheRightEffect::Disable() {
-	for (CVehicle* vehicle : CPools::ms_pVehiclePool) {
-		int amplify = 10;
+	if (!Config::GetOrDefault("CrowdControl.Enabled", false)) {
+		for (CVehicle* vehicle : CPools::ms_pVehiclePool) {
+			int amplify = 10;
 
-		vehicle->m_vecMoveSpeed.x += RandomHelper::Random(-0.25f, 0.25f, amplify);
-		vehicle->m_vecMoveSpeed.y += RandomHelper::Random(-0.25f, 0.25f, amplify);
-		vehicle->m_vecMoveSpeed.z += RandomHelper::Random(-0.05f, 0.05f, amplify);
+			vehicle->m_vecMoveSpeed.x += RandomHelper::Random(-0.25f, 0.25f, amplify);
+			vehicle->m_vecMoveSpeed.y += RandomHelper::Random(-0.25f, 0.25f, amplify);
+			vehicle->m_vecMoveSpeed.z += RandomHelper::Random(-0.05f, 0.05f, amplify);
+		}
 	}
 
 	EffectBase::Disable();
