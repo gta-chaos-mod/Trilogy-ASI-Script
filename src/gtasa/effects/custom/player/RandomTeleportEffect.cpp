30de893e57e363ef066f5996f23422299dc1b68c
Lordmau5 <mail@lordmau5.com>
2020-08-22 10:17:02 +0200

    SA/Fix: `Random Teleport` now properly calculating height

--- src/gtasa/effects/custom/player/RandomTeleportEffect.cpp
+++ src/gtasa/effects/custom/player/RandomTeleportEffect.cpp
@@ -1,5 +1,7 @@
 #include "RandomTeleportEffect.h"
 
+#include "extensions/ScriptCommands.h"
+
 RandomTeleportEffect::RandomTeleportEffect () : EffectBase ("effect_random_teleport")
 {
 }
@@ -14,9 +16,22 @@ RandomTeleportEffect::HandleTick ()
     {
         CVector randomPosition
             = CVector (RandomHelper::Random (-3000.0f, 3000.0f),
-                       RandomHelper::Random (-3000.0f, 3000.0f), 0.0f);
+                       RandomHelper::Random (-3000.0f, 3000.0f), 20.0f);
 
-        // TODO: Get highest point for X, Y coordinates
+        Command<eScriptCommands::COMMAND_REQUEST_COLLISION> (randomPosition.x, randomPosition.y);
+        CStreaming::StreamZoneModels (&randomPosition);
+        CStreaming::LoadAllRequestedModels (false);
+        CStreaming::LoadScene (&randomPosition);
+
+        randomPosition.z = CWorld::FindGroundZForCoord (randomPosition.x,
+                                                        randomPosition.y) + 3.0f;
+
+        float pLevel = 0;
+        Call<0x6EB690> (randomPosition.x, randomPosition.y, randomPosition.z, &pLevel, false, 1); // CWaterLevel::GetWaterLevel
+        if (randomPosition.z < pLevel)
+        {
+            randomPosition.z = pLevel + 3.0f;
+        }
 
         Teleportation::Teleport (randomPosition);
 
