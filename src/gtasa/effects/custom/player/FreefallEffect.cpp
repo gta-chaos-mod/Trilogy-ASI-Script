da8c9d5785e8ab3896f6f00523c6bec59990511f
Lordmau5 <mail@lordmau5.com>
2020-09-11 12:04:44 +0200

    SA/Fix: Teleport player to proper interior with "Freefall"

--- src/gtasa/effects/custom/player/FreefallEffect.cpp
+++ src/gtasa/effects/custom/player/FreefallEffect.cpp
@@ -26,13 +26,13 @@ FreefallEffect::HandleTick ()
         position.z += 1249.01189998819991197253f; // "1250 is too much of an
                                                   // even number" ~ Megumi_TV
 
-        Teleportation::Teleport (position);
+        Teleportation::Teleport (position, player->m_nAreaCode);
 
         Disable ();
     }
     else
     {
-        if (Teleportation::CanTeleport())
+        if (Teleportation::CanTeleport ())
         {
             CVehicle *playerVehicle = FindPlayerVehicle (-1, false);
             if (playerVehicle)
@@ -48,9 +48,11 @@ FreefallEffect::HandleTick ()
                 CCheat::ParachuteCheat ();
 
                 CVector position = player->GetPosition ();
-                position.z += 1249.01189998819991197253f; // "1250 is too much of an even number" ~ Megumi_TV
+                position.z
+                    += 1249.01189998819991197253f; // "1250 is too much of an
+                                                   // even number" ~ Megumi_TV
 
-                Teleportation::Teleport (position);
+                Teleportation::Teleport (position, player->m_nAreaCode);
 
                 Disable ();
             }
