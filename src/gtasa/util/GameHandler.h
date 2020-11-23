f2ed0359b7ef437763fe8b3df5095eddefd03487
Lordmau5 <mail@lordmau5.com>
2020-11-23 17:23:59 +0100

    Remove stupid and silly gangwar autosave code
    Can just disable the need for gang territories in the config now. Use it.

--- src/gtasa/util/GameHandler.h
+++ src/gtasa/util/GameHandler.h
@@ -32,7 +32,6 @@ public:
 
     static void __fastcall HookedGenericLoadTheScriptsLoad ();
     static void __fastcall HookedOnGangWarHoodCaptured ();
-    static void HookedCGangWarsUpdate ();
     static __int16 __fastcall HookedFinaleGetGangTerritories (
         CRunningScript *thisScript, void *edx, __int16 count);
     static double HookedOpCodeGetStatValue (int statid);
