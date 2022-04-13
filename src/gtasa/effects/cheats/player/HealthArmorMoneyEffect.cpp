e70a700aca5c407ddca069f6ab40449b19edd107
Lordmau5 <mail@lordmau5.com>
2022-04-13 16:30:15 +0200

    Add "Health, Armor & Money" effect to SA

---
+++ src/gtasa/effects/cheats/player/HealthArmorMoneyEffect.cpp
@@ -0,0 +1,33 @@
+#include <effects/OneTimeEffect.h>
+
+#include "CPlayerInfo.h"
+
+class HealthArmorMoneyEffect : public OneTimeEffect
+{
+public:
+    void
+    OnStart (EffectInstance *inst) override
+    {
+        CPlayerPed *player = FindPlayerPed ();
+        if (player)
+        {
+            CPlayerInfo *playerInfo = player->GetPlayerInfoForThisPlayerPed ();
+
+            player->m_fHealth = player->m_fMaxHealth;
+            player->m_fArmour = playerInfo->m_nMaxArmour;
+            playerInfo->m_nMoney += 250000;
+
+            CVehicle *vehicle = FindPlayerVehicle (-1, false);
+            if (vehicle)
+            {
+                float setHealth
+                    = vehicle->m_fHealth > 1000.0f ? 100000.0f : 1000.0f;
+                vehicle->Fix ();
+                vehicle->m_fHealth = setHealth;
+            }
+        }
+    }
+};
+
+DEFINE_EFFECT (HealthArmorMoneyEffect, "effect_health_armor_money",
+               GROUP_HEALTH);
