22c70e0a6825cb3bf0d52d198bcae91be9043bc9
Lordmau5 <mail@lordmau5.com>
2022-04-14 18:20:22 +0200

    Add more effects
    (The Flash is the new name for Usain Bolt)

---
+++ src/gtasa/effects/custom/player/FreefallEffect.cpp
@@ -0,0 +1,45 @@
+#include <util/EffectBase.h>
+#include <util/Teleportation.h>
+
+#include "CTaskSimpleCarSetPedOut.h"
+
+class FreefallEffect : public EffectBase
+{
+public:
+    bool
+    CanActivate () override
+    {
+        return Teleportation::CanTeleport ();
+    }
+
+    void
+    OnTick (EffectInstance *inst) override
+    {
+        CPlayerPed *player = FindPlayerPed ();
+        if (!player || !CanActivate ())
+        {
+            return;
+        }
+
+        CVehicle *playerVehicle = FindPlayerVehicle (-1, false);
+        if (playerVehicle)
+        {
+            player->m_pIntelligence->m_TaskMgr.SetTask (
+                new CTaskSimpleCarSetPedOut (playerVehicle, 0, false), 0,
+                false);
+
+            return;
+        }
+
+        CCheat::ParachuteCheat ();
+
+        CVector position = player->GetPosition ();
+        position.z       = 1250.0f;
+
+        Teleportation::Teleport (position);
+
+        inst->Disable ();
+    }
+};
+
+DEFINE_EFFECT (FreefallEffect, "effect_freefall", 0);
---
+++ src/gtasa/effects/custom/player/TheFlashEffect.cpp
@@ -0,0 +1,81 @@
+#include <util/EffectBase.h>
+
+#include "CMenuSystem.h"
+#include "CWorld.h"
+
+// TODO: Hook CEventDamage_AffectsPed for fall damage
+// HookCall (0x5E2F57, Hooked_CEventDamage_AffectsPed);
+// HookCall (0x5E3020, Hooked_CEventDamage_AffectsPed);
+
+/*
+bool __fastcall UsainBoltEffect::Hooked_CEventDamage_AffectsPed (
+    CEventDamage *thisEventDamage, void *edx, CPed *ped)
+{
+    return false;
+}
+*/
+
+class TheFlashEffect : public EffectBase
+{
+    bool oldInfiniteRun = false;
+
+public:
+    void
+    OnStart (EffectInstance *inst) override
+    {
+        this->oldInfiniteRun = injector::ReadMemory<bool> (0xB7CEE4);
+        injector::WriteMemory (0xB7CEE4, true);
+    }
+
+    void
+    OnEnd (EffectInstance *inst) override
+    {
+        injector::WriteMemory (0x8D2458, 5.0f);
+        injector::WriteMemory (0xB7CEE4, this->oldInfiniteRun);
+    }
+
+    void
+    OnTick (EffectInstance *inst) override
+    {
+        CPlayerPed *player = FindPlayerPed ();
+        if (player)
+        {
+            // Infinite Lung Capacity
+            player->ResetPlayerBreath ();
+
+            CPad *pad = player->GetPadFromPlayer ();
+            if (pad)
+            {
+                // Auto Super Sprint
+                bool canFastSprint
+                    = pad->GetSprint ()
+                      && !CMenuSystem::MenuInUse[CMenuSystem::CurrentMenuInUse];
+
+                if (canFastSprint)
+                {
+                    pad->NewState.ButtonCross = 0;
+
+                    if (player->m_pIntelligence->GetTaskSwim () == nullptr)
+                    {
+                        // Update Z Pos To Ground Pos
+                        CVector pos    = player->GetMatrix ()->pos;
+                        bool    worked = false;
+                        float   newZ
+                            = CWorld::FindGroundZFor3DCoord (pos.x, pos.y,
+                                                             pos.z, &worked,
+                                                             nullptr);
+                        if (worked)
+                        {
+                            player->GetMatrix ()->pos.z
+                                = std::max (0.0f, newZ + 0.5f);
+                        }
+                    }
+                }
+
+                injector::WriteMemory (0x8D2458, canFastSprint ? 0.25f : 5.0f);
+            }
+        }
+    }
+};
+
+DEFINE_EFFECT (TheFlashEffect, "effect_the_flash", 0);
