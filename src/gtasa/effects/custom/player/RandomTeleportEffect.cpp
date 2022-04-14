3cb8be65771465b993dc4c2673ea0358b742f76e
Lordmau5 <mail@lordmau5.com>
2022-04-14 18:19:40 +0200

    Fix `Random Teleport` teleporting the player on water

--- src/gtasa/effects/custom/player/RandomTeleportEffect.cpp
+++ src/gtasa/effects/custom/player/RandomTeleportEffect.cpp
@@ -22,33 +22,49 @@ public:
         CPlayerPed *player = FindPlayerPed ();
         if (player && CanActivate ())
         {
-            CVector randomPosition
-                = CVector (inst->Random (-3000.0f, 3000.0f),
-                           inst->Random (-3000.0f, 3000.0f), 20.0f);
-
-            Command<eScriptCommands::COMMAND_REQUEST_COLLISION> (
-                randomPosition.x, randomPosition.y);
-            CStreaming::StreamZoneModels (&randomPosition);
-            CStreaming::LoadAllRequestedModels (false);
-            CStreaming::LoadScene (&randomPosition);
-
-            randomPosition.z = CWorld::FindGroundZForCoord (randomPosition.x,
-                                                            randomPosition.y)
-                               + 3.0f;
-
-            float pLevel = 0;
-            Call<0x6EB690> (randomPosition.x, randomPosition.y,
-                            randomPosition.z, &pLevel, false,
-                            nullptr); // CWaterLevel::GetWaterLevel
-            if (randomPosition.z < pLevel)
-            {
-                randomPosition.z = pLevel + 3.0f;
-            }
+            CVector randomPosition = FindSuitableTeleportPosition (inst);
 
             Teleportation::Teleport (randomPosition);
 
             inst->Disable ();
         }
+    }
+
+    CVector
+    FindSuitableTeleportPosition (EffectInstance *inst)
+    {
+        CVector randomPosition
+            = CVector (inst->Random (-3000.0f, 3000.0f),
+                       inst->Random (-3000.0f, 3000.0f), 0.0f);
+
+        Command<eScriptCommands::COMMAND_REQUEST_COLLISION> (randomPosition.x,
+                                                             randomPosition.y);
+        CStreaming::StreamZoneModels (&randomPosition);
+        CStreaming::LoadAllRequestedModels (false);
+        CStreaming::LoadScene (&randomPosition);
+
+        bool groundResult = false;
+        randomPosition.z
+            = CWorld::FindGroundZFor3DCoord (randomPosition.x, randomPosition.y,
+                                             randomPosition.z + 250.0f,
+                                             &groundResult, 0)
+              + 3.0f;
+
+        if (!groundResult)
+        {
+            return FindSuitableTeleportPosition (inst);
+        }
+
+        float waterLevel = 0.0f;
+        Command<eScriptCommands::COMMAND_GET_WATER_HEIGHT_AT_COORDS> (
+            randomPosition.x, randomPosition.y, true, &waterLevel);
+
+        if (randomPosition.z <= waterLevel)
+        {
+            return FindSuitableTeleportPosition (inst);
+        }
+
+        return randomPosition;
     }
 };
 
