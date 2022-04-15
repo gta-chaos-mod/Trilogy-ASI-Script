7789736e4ffa6cf690007f2fb98ec4775d8c6d8e
Lordmau5 <mail@lordmau5.com>
2022-04-15 23:20:51 +0200

    Fix Vehicle Size effects, also add a few more

--- src/gtasa/effects/custom/vehicle/VehicleSizeEffects.cpp
+++ src/gtasa/effects/custom/vehicle/VehicleSizeEffects.cpp
@@ -43,31 +43,33 @@ public:
     }
 };
 
-template <RwV3d scale, RwV3d rotation>
-class VehicleSizeContinuousRotation
-    : public VehicleSizeEffect<scale, rotation, 360.0f>
-{
-    using Base = VehicleSizeEffect<scale, rotation, 360.0f>;
-
-public:
-    void
-    OnTick (EffectInstance *inst) override
-    {
-        Base::SetRotationAngle (
-            fmod (Base::GetRotationAngle () + 30.0f, 360.0f));
-    }
-};
-
 // clang-format off
-using VehicleSizeBackwardsEffect = VehicleSizeEffect<RwV3d {1.0f, 1.0f, 1.0f}, RwV3d {0.0f, 0.0f, 1.0f}, 180.0f>;
-DEFINE_EFFECT (VehicleSizeBackwardsEffect, "effect_vehicle_size_backwards", 0);
+// Sizes / Scales
+using VehicleSizeTinyCarsEffect = VehicleSizeEffect<RwV3d {0.5f, 0.5f, 0.5f}, RwV3d {0.0f, 0.0f, 0.0f}, 0.0f>;
+DEFINE_EFFECT (VehicleSizeTinyCarsEffect, "effect_vehicle_size_tiny", 0);
+
+using VehicleSizeLargeCarsEffect = VehicleSizeEffect<RwV3d {2.0f, 2.0f, 2.0f}, RwV3d {0.0f, 0.0f, 0.0f}, 0.0f>;
+DEFINE_EFFECT (VehicleSizeLargeCarsEffect, "effect_vehicle_size_large", 0);
 
 using VehicleSizeWideEffect = VehicleSizeEffect<RwV3d {2.0f, 1.0f, 1.0f}, RwV3d {0.0f, 0.0f, 0.0f}, 0.0f>;
 DEFINE_EFFECT (VehicleSizeWideEffect, "effect_vehicle_size_wide", 0);
 
-using VehicleSizeFlippedEffect = VehicleSizeEffect<RwV3d {1.0f, 1.0f, 1.0f}, RwV3d {0.0f, 1.0f, 0.0f}, 180.0f>;
-DEFINE_EFFECT (VehicleSizeFlippedEffect, "effect_vehicle_size_flipped", 0);
+using VehicleSizeSuperWideEffect = VehicleSizeEffect<RwV3d {3.0f, 1.0f, 1.0f}, RwV3d {0.0f, 0.0f, 0.0f}, 0.0f>;
+DEFINE_EFFECT (VehicleSizeSuperWideEffect, "effect_vehicle_size_super_wide", 0);
+
+using VehicleSizeTallEffect = VehicleSizeEffect<RwV3d {1.0f, 1.0f, 2.0f}, RwV3d {0.0f, 0.0f, 0.0f}, 0.0f>;
+DEFINE_EFFECT (VehicleSizeTallEffect, "effect_vehicle_size_tall", 0);
+
+using VehicleSizeLongEffect = VehicleSizeEffect<RwV3d {1.0f, 2.0f, 1.0f}, RwV3d {0.0f, 0.0f, 0.0f}, 0.0f>;
+DEFINE_EFFECT (VehicleSizeLongEffect, "effect_vehicle_size_long", 0);
 
 using VehicleSizePaperThinEffect = VehicleSizeEffect<RwV3d {0.01f, 1.0f, 1.0f}, RwV3d {0.0f, 0.0f, 0.0f}, 0.0f>;
 DEFINE_EFFECT (VehicleSizePaperThinEffect, "effect_vehicle_size_paper_thin", 0);
+
+// Rotations
+using VehicleSizeBackwardsEffect = VehicleSizeEffect<RwV3d {1.0f, 1.0f, 1.0f}, RwV3d {0.0f, 0.0f, 1.0f}, 180.0f>;
+DEFINE_EFFECT (VehicleSizeBackwardsEffect, "effect_vehicle_size_backwards", 0);
+
+using VehicleSizeFlippedEffect = VehicleSizeEffect<RwV3d {1.0f, 1.0f, 1.0f}, RwV3d {0.0f, 1.0f, 0.0f}, 180.0f>;
+DEFINE_EFFECT (VehicleSizeFlippedEffect, "effect_vehicle_size_flipped", 0);
 // clang-format on
