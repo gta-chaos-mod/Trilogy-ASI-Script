efe99bb9cbab972fd93a5cfd7c7b52da8e229183
Lordmau5 <mail@lordmau5.com>
2022-04-25 22:39:13 +0200

    Fix `Timelapse` saving the wrong value when active

--- src/gtasa/effects/custom/time/TimelapseEffect.cpp
+++ src/gtasa/effects/custom/time/TimelapseEffect.cpp
@@ -2,19 +2,40 @@
 
 #include <CClock.h>
 
+using namespace plugin;
+
 class TimelapseEffect : public EffectBase
 {
 public:
     void
+    OnStart (EffectInstance *inst) override
+    {
+        injector::MakeCALL (0x5D14AE,
+                            Hooked_CSimpleVariablesSaveStructure_Construct);
+    }
+
+    void
     OnEnd (EffectInstance *inst) override
     {
         CClock::ms_nMillisecondsPerGameMinute = 1000;
+
+        injector::MakeCALL (0x5D14AE, 0x5D1B80);
     }
 
     void
     OnTick (EffectInstance *inst) override
     {
         CClock::ms_nMillisecondsPerGameMinute = 10;
+    }
+
+    static int __fastcall Hooked_CSimpleVariablesSaveStructure_Construct (
+        void *thisStructure)
+    {
+        CClock::ms_nMillisecondsPerGameMinute = 1000;
+        int result = CallMethodAndReturn<int, 0x5D1B80, void *> (thisStructure);
+        CClock::ms_nMillisecondsPerGameMinute = 10;
+
+        return result;
     }
 };
 
