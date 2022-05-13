7033e4c1e758024e51ed2d73dac0892684d1c2f9
Lordmau5 <mail@lordmau5.com>
2022-05-13 23:19:38 +0200

    Reset weather once `Weather` effect runs out

--- src/gtasa/effects/generic/WeatherEffect.cpp
+++ src/gtasa/effects/generic/WeatherEffect.cpp
@@ -4,14 +4,23 @@
 
 class WeatherEffect : public EffectBase
 {
-    eWeatherType weather = eWeatherType::WEATHER_EXTRASUNNY_LA;
+    eWeatherType oldWeather = eWeatherType::WEATHER_EXTRASUNNY_LA;
+    eWeatherType weather    = eWeatherType::WEATHER_EXTRASUNNY_LA;
 
 public:
     void
     OnStart (EffectInstance *inst) override
     {
+        this->oldWeather = (eWeatherType) CWeather::OldWeatherType;
+
         this->weather
             = (eWeatherType) inst->GetCustomData ().value ("weatherID", 0);
+    }
+
+    void
+    OnEnd (EffectInstance *inst) override
+    {
+        CWeather::ForceWeatherNow (this->oldWeather);
     }
 
     void
