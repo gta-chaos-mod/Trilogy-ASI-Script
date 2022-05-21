9ad2aaf8abd370564aec609edb44e90f5e1604d9
Lordmau5 <mail@lordmau5.com>
2022-05-21 13:32:02 +0200

    Add new effect `Spawn Ramp`

---
+++ src/gtasa/effects/custom/unsorted/SpawnRampEffect.cpp
@@ -0,0 +1,39 @@
+#include "util/EffectBase.h"
+
+#include <CStreaming.h>
+#include <extensions/ScriptCommands.h>
+
+using namespace plugin;
+
+class SpawnRampEffect : public EffectBase
+{
+    CObject *rampObject;
+
+public:
+    void
+    OnStart (EffectInstance *inst) override
+    {
+        CPlayerPed *player = FindPlayerPed ();
+        if (!player) return;
+
+        CVector position
+            = player->TransformFromObjectSpace (CVector (0.0f, 10.0f, -1.2f));
+
+        int model = 1634; // Ramp
+        CStreaming::RequestModel (model, 2);
+        CStreaming::LoadAllRequestedModels (0);
+        Command<eScriptCommands::COMMAND_CREATE_OBJECT> (model, position.x,
+                                                         position.y, position.z,
+                                                         &rampObject);
+        CStreaming::SetModelIsDeletable (model);
+    }
+
+    void
+    OnEnd (EffectInstance *inst) override
+    {
+        if (rampObject && IsObjectPointerValid (rampObject))
+            rampObject->Remove ();
+    }
+};
+
+DEFINE_EFFECT (SpawnRampEffect, "effect_spawn_ramp", 0);
