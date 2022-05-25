185a9ed0b2d73a84c14f25a3e12f53beff066e79
Lordmau5 <mail@lordmau5.com>
2022-05-25 22:46:41 +0200

    Disable `Freeze Vehicle` when not in a vehicle

--- src/gtasa/effects/custom/vehicle/FreezeVehicleEffect.cpp
+++ src/gtasa/effects/custom/vehicle/FreezeVehicleEffect.cpp
@@ -19,33 +19,34 @@ public:
     OnStart (EffectInstance *inst) override
     {
         vehicle = FindPlayerVehicle (-1, false);
-        if (vehicle)
+        if (!vehicle)
         {
-            position  = vehicle->GetPosition ();
-            moveSpeed = vehicle->m_vecMoveSpeed;
-            turnSpeed = vehicle->m_vecTurnSpeed;
+            inst->Disable ();
+            return;
         }
+
+        position  = vehicle->GetPosition ();
+        moveSpeed = vehicle->m_vecMoveSpeed;
+        turnSpeed = vehicle->m_vecTurnSpeed;
     }
 
     void
     OnEnd (EffectInstance *inst) override
     {
-        if (vehicle)
-        {
-            vehicle->m_vecMoveSpeed = moveSpeed;
-            vehicle->m_vecTurnSpeed = turnSpeed;
-        }
+        if (!vehicle) return;
+
+        vehicle->m_vecMoveSpeed = moveSpeed;
+        vehicle->m_vecTurnSpeed = turnSpeed;
     }
 
     void
     OnTick (EffectInstance *inst) override
     {
-        if (vehicle)
-        {
-            vehicle->SetPosn (position);
-            vehicle->m_vecMoveSpeed = {0, 0, 0};
-            vehicle->m_vecTurnSpeed = {0, 0, 0};
-        }
+        if (!vehicle) return;
+
+        vehicle->SetPosn (position);
+        vehicle->m_vecMoveSpeed = {0, 0, 0};
+        vehicle->m_vecTurnSpeed = {0, 0, 0};
     }
 };
 
