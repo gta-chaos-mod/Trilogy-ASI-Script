8e01880100b6c28ccf5f00467d6035e54118d4f2
Lordmau5 <mail@lordmau5.com>
2022-05-28 13:00:06 +0200

    Potentially fix game crash with cop cars for `Custom Vehicle Spawns`

--- src/gtasa/effects/custom/vehicle/CustomVehicleSpawnsEffect.cpp
+++ src/gtasa/effects/custom/vehicle/CustomVehicleSpawnsEffect.cpp
@@ -21,18 +21,17 @@ public:
         HOOK_ARGS (inst, Hooked_RandomizeTrafficCars, int (int *), 0x43022A);
         HOOK (inst, Hooked_RandomizeCarToLoad, int (int *), 0x40B4CB, 0x40B596,
               0x40B62F, 0x40ED07);
+        HOOK (inst, Hooked_ChoosePoliceCarModel, int (int), 0x424E20, 0x42C320,
+              0x43020E, 0x430283);
 
-        HOOK_ARGS (inst, FixEmptyPoliceCars, void (uint8_t *, char), 0x42BC26,
-                   0x42C620, 0x431EE5, 0x499CBB, 0x499D6A, 0x49A5EB, 0x49A85E,
-                   0x49A9AF);
-
-        HOOK (inst, RandomizeCarToLoad, int (int), 0x40B4CB, 0x40B596, 0x40B62F,
-              0x40ED07);
+        HOOK_ARGS (inst, Hooked_FixEmptyPoliceCars, void (uint8_t *, char),
+                   0x42BC26, 0x42C620, 0x431EE5, 0x499CBB, 0x499D6A, 0x49A5EB,
+                   0x49A85E, 0x49A9AF);
 
         // Parked Cars
-        HOOK_METHOD_ARGS (inst, RandomizeFixedSpawn, void (CCarGenerator *),
-                          0x6F3EC1);
-        HOOK_METHOD (inst, RandomizeRandomSpawn,
+        HOOK_METHOD_ARGS (inst, Hooked_RandomizeFixedSpawn,
+                          void (CCarGenerator *), 0x6F3EC1);
+        HOOK_METHOD (inst, Hooked_RandomizeRandomSpawn,
                      int (CLoadedCarGroup *, char, char), 0x6F3583);
     }
 
@@ -91,8 +90,16 @@ public:
         return vehicleID;
     }
 
+    static int
+    Hooked_ChoosePoliceCarModel (auto &&cb)
+    {
+        LoadCarModel ();
+
+        return vehicleID;
+    }
+
     static void
-    FixEmptyPoliceCars (auto &&cb, uint8_t *vehicle, char a3)
+    Hooked_FixEmptyPoliceCars (auto &&cb, uint8_t *vehicle, char a3)
     {
         uint16_t *modelIndex     = (uint16_t *) (vehicle + 0x22);
         uint16_t  original_index = *modelIndex;
@@ -112,7 +119,7 @@ public:
     }
 
     static void
-    RandomizeFixedSpawn (auto &&cb, CCarGenerator *carGen)
+    Hooked_RandomizeFixedSpawn (auto &&cb, CCarGenerator *carGen)
     {
         short oldModel = carGen->m_nModelId;
 
@@ -122,7 +129,7 @@ public:
     }
 
     static int
-    RandomizeRandomSpawn (auto &&cb)
+    Hooked_RandomizeRandomSpawn (auto &&cb)
     {
         return vehicleID;
     }
