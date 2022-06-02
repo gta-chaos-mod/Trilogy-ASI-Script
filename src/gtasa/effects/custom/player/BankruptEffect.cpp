af9bdcfc5f335624fbd83ac2f28724b00b3d053a
Lordmau5 <mail@lordmau5.com>
2022-06-02 14:02:17 +0200

    Refactor an effect

--- src/gtasa/effects/custom/player/BankruptEffect.cpp
+++ src/gtasa/effects/custom/player/BankruptEffect.cpp
@@ -9,17 +9,15 @@ public:
     OnTick (EffectInstance *inst) override
     {
         CPlayerPed *player = FindPlayerPed ();
-        if (player)
-        {
-            CPlayerInfo *playerInfo = player->GetPlayerInfoForThisPlayerPed ();
-            if (playerInfo)
-            {
-                // -= 1 million
-                playerInfo->m_nMoney -= 1000000;
-                playerInfo->m_nMoney = std::max (playerInfo->m_nMoney, 0);
-                inst->Disable ();
-            }
-        }
+        if (!player) return;
+
+        CPlayerInfo *playerInfo = player->GetPlayerInfoForThisPlayerPed ();
+        if (!playerInfo) return;
+
+        // -= 1 million
+        playerInfo->m_nMoney -= 1000000;
+        playerInfo->m_nMoney = std::max (playerInfo->m_nMoney, 0);
+        inst->Disable ();
     }
 };
 
