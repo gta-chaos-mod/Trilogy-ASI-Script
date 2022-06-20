676a45e3a1ffa4e5999551476a576fe6a35539e3
Lordmau5 <mail@lordmau5.com>
2022-06-20 17:14:29 +0200

    Refactor `Delayed Screen` into `Delayed Controls`
    Not 100% perfect but WAY better than storing frames into memory

---
+++ src/gtasa/effects/custom/unsorted/DelayedControlsEffect.cpp
@@ -0,0 +1,79 @@
+#include "util/EffectBase.h"
+
+#include <deque>
+
+#include <CMenuManager.h>
+#include <CTimer.h>
+
+class DelayedControls : public EffectBase
+{
+    struct InputData
+    {
+        CControllerState      state;
+        CMouseControllerState mouseState;
+        CKeyboardState        keyboardState;
+    };
+
+    bool                  wasFrameLimiterOn = false;
+    std::deque<InputData> bufferedInputData = {};
+
+public:
+    void
+    OnStart (EffectInstance *inst) override
+    {
+        bufferedInputData.clear ();
+        wasFrameLimiterOn = FrontEndMenuManager.m_bFrameLimiterOn;
+    }
+
+    void
+    OnProcessScripts (EffectInstance *inst) override
+    {
+        CPlayerPed *player = FindPlayerPed ();
+        if (!player) return;
+
+        CPad *pad = player->GetPadFromPlayer ();
+        if (!pad) return;
+
+        if (wasFrameLimiterOn != FrontEndMenuManager.m_bFrameLimiterOn)
+            bufferedInputData.clear ();
+
+        wasFrameLimiterOn = FrontEndMenuManager.m_bFrameLimiterOn;
+
+        InputData frameData = {.state         = pad->NewState,
+                               .mouseState    = pad->NewMouseControllerState,
+                               .keyboardState = pad->NewKeyState};
+
+        bufferedInputData.push_back (frameData);
+        if (bufferedInputData.size () > GetFrameDelay ())
+        {
+            pad->OldState                = pad->NewState;
+            pad->OldMouseControllerState = pad->NewMouseControllerState;
+            pad->OldKeyState             = pad->NewKeyState;
+
+            InputData data               = bufferedInputData.front ();
+            pad->NewState                = data.state;
+            pad->NewMouseControllerState = data.mouseState;
+            pad->NewKeyState             = data.keyboardState;
+
+            bufferedInputData.pop_front ();
+        }
+        else
+        {
+            pad->NewState                = CControllerState ();
+            pad->NewMouseControllerState = CMouseControllerState ();
+            pad->NewKeyState             = CKeyboardState ();
+
+            pad->OldState                = CControllerState ();
+            pad->OldMouseControllerState = CMouseControllerState ();
+            pad->OldKeyState             = CKeyboardState ();
+        }
+    }
+
+    static int
+    GetFrameDelay ()
+    {
+        return (int) std::floor (CTimer::game_FPS / 5.0f);
+    }
+};
+
+DEFINE_EFFECT (DelayedControls, "effect_delayed_controls", GROUP_CONTROLS);
