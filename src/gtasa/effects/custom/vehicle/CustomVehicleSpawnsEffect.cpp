3b87598729bda9f8904ca11dd974d30707df8bff
Lordmau5 <mail@lordmau5.com>
2022-06-25 14:19:58 +0200

    Fix `Vehicle Spawns` effects (thanks Parik ♥)

--- src/gtasa/effects/custom/vehicle/CustomVehicleSpawnsEffect.cpp
+++ src/gtasa/effects/custom/vehicle/CustomVehicleSpawnsEffect.cpp
@@ -1,3 +1,6 @@
+// Snippets and some logic taken from the Rainbomizer mod
+// https://github.com/Parik27/SA.Rainbomizer/blob/master/src/traffic.cc
+
 #include "util/EffectBase.h"
 #include "util/hooks/HookMacros.h"
 
@@ -5,14 +8,12 @@
 #include <CCarCtrl.h>
 #include <CCarGenerator.h>
 #include <CLoadedCarGroup.h>
+#include <CModelInfo.h>
+#include <CPopulation.h>
 #include <CStreaming.h>
 
 using namespace plugin;
 
-// TODO: Cop bikes and helicopters still spawn
-// The game can also crash in regards to cops sometimes
-
-// Cop cars will spawn normally for now
 template <int vehicleID> class CustomVehicleSpawnsEffect : public EffectBase
 {
 public:
@@ -21,24 +22,31 @@ public:
     {
         // Traffic
         HOOK_ARGS (inst, Hooked_RandomizeTrafficCars, int (int *), 0x43022A);
+
         HOOK (inst, Hooked_RandomizeCarToLoad, int (int *), 0x40B4CB, 0x40B596,
               0x40B62F, 0x40ED07);
-        // HOOK (inst, Hooked_ChoosePoliceCarModel, int (int), 0x424E20,
-        // 0x42C320,
-        //       0x43020E, 0x430283);
 
-        // HOOK_ARGS (inst, Hooked_FixEmptyPoliceCars, void (uint8_t *, char),
-        //            0x42BC26, 0x42C620, 0x431EE5, 0x499CBB, 0x499D6A,
-        //            0x49A5EB, 0x49A85E, 0x49A9AF);
+        HOOK (inst, Hooked_ChoosePoliceCarModel, int (int), 0x424E20, 0x42C320,
+              0x43020E, 0x430283);
 
-        // HOOK_ARGS (inst, Hooked_FixCopCrash, CCopPed * (CCopPed *, eCopType),
-        //            0x61282F);
+        HOOK_ARGS (inst, Hooked_FixEmptyPoliceCars, void (uint8_t *, char),
+                   0x42BC26, 0x42C620, 0x431EE5, 0x499CBB, 0x499D6A, 0x49A5EB,
+                   0x49A85E, 0x49A9AF);
+
+        HOOK_METHOD_ARGS (inst, Hooked_FixCopCrash,
+                          CCopPed * (CCopPed *, eCopType), 0x61282F);
 
         // Parked Cars
         HOOK_METHOD_ARGS (inst, Hooked_RandomizeFixedSpawn,
                           void (CCarGenerator *), 0x6F3EC1);
+
         HOOK_METHOD (inst, Hooked_RandomizeRandomSpawn,
                      int (CLoadedCarGroup *, char, char), 0x6F3583);
+
+        // Roadblocks
+        HOOK_METHOD_ARGS (inst, Hooked_RandomizeRoadblocks,
+                          CVehicle * (CVehicle *, int, char, char), 0x462217,
+                          0x4998F0, 0x42B909);
     }
 
     static bool
@@ -109,17 +117,19 @@ public:
     static void
     Hooked_FixEmptyPoliceCars (auto &&cb, uint8_t *vehicle, char a3)
     {
+        LoadCarModel ();
+
         uint16_t *modelIndex     = (uint16_t *) (vehicle + 0x22);
         uint16_t  original_index = *modelIndex;
 
-        *modelIndex = ChoosePoliceVehicleBasedOnModel (original_index);
+        *modelIndex = ChoosePoliceVehicleBasedOnModel ();
         CCarAI::AddPoliceCarOccupants ((CVehicle *) vehicle, a3);
 
         *modelIndex = original_index; // restore original model
     }
 
     static int
-    ChoosePoliceVehicleBasedOnModel (int model)
+    ChoosePoliceVehicleBasedOnModel ()
     {
         if (vehicleID != 528 && vehicleID != 601 && IsPoliceModel (vehicleID))
             return vehicleID;
@@ -128,6 +138,7 @@ public:
         int seats = CallAndReturn<int, 0x4C89B0> (vehicleID);
         switch (seats)
         {
+            case 0: return 523;
             case 1: return 523;
             case 2: return 599;
             default: return 490;
@@ -139,9 +150,11 @@ public:
     {
         type = COP_TYPE_CITYCOP;
 
-        for (auto i : {std::make_pair (287, COP_TYPE_FBI),
-                       std::make_pair (286, COP_TYPE_FBI),
-                       std::make_pair (285, COP_TYPE_SWAT2)})
+        for (auto i : {
+                 std::make_pair (285, COP_TYPE_SWAT2),
+                 std::make_pair (286, COP_TYPE_FBI),
+                 std::make_pair (287, COP_TYPE_ARMY),
+             })
         {
             if (CStreamingInfo::ms_pArrayBase[i.first].m_nLoadState == 1)
             {
@@ -150,7 +163,7 @@ public:
             }
         }
 
-        return new CCopPed (type);
+        return cb ();
     }
 
     static int
@@ -175,6 +188,47 @@ public:
     Hooked_RandomizeRandomSpawn (auto &&cb)
     {
         return vehicleID;
+    }
+
+    static CVehicle *
+    Hooked_RandomizeRoadblocks (auto &&cb, CVehicle *vehicle, int &modelIndex,
+                                char createdBy, char setupSuspensionLines)
+    {
+        modelIndex = vehicleID;
+
+        if (CModelInfo::IsBoatModel (modelIndex)
+            || CModelInfo::IsTrainModel (modelIndex))
+            CallMethod<0x6F2940> (vehicle, modelIndex, createdBy);
+
+        else if (CModelInfo::IsPlaneModel (modelIndex))
+            CallMethod<0x6C8E20> (vehicle, modelIndex, createdBy);
+
+        else if (CModelInfo::IsHeliModel (modelIndex))
+            CallMethod<0x6C4190> (vehicle, modelIndex, createdBy);
+
+        else if (CModelInfo::IsBikeModel (modelIndex))
+            CallMethod<0x6BF430> (vehicle, modelIndex, createdBy);
+
+        else if (CModelInfo::IsBmxModel (modelIndex))
+            CallMethod<0x6BF820> (vehicle, modelIndex, createdBy);
+
+        else if (CModelInfo::IsTrailerModel (modelIndex))
+            CallMethod<0x6D03A0> (vehicle, modelIndex, createdBy);
+
+        else if (CModelInfo::IsQuadBikeModel (modelIndex))
+            CallMethod<0x6CE370> (vehicle, modelIndex, createdBy);
+
+        else if (CModelInfo::IsMonsterTruckModel (modelIndex))
+            CallMethod<0x6C8D60> (vehicle, modelIndex, createdBy);
+
+        else if (CModelInfo::IsCarModel (modelIndex))
+            CallMethod<0x6B0A90> (vehicle, modelIndex, createdBy,
+                                  setupSuspensionLines);
+
+        else
+            cb ();
+
+        return vehicle;
     }
 };
 
