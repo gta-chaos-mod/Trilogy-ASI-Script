1a2e6fefa6e49a39deafadc5a760ce04f7366ff6
Lordmau5 <mail@lordmau5.com>
2022-06-26 00:35:25 +0200

    Add new effect `Struck By Truck`

---
+++ src/gtasa/effects/custom/player/StruckByTruckEffect.cpp
@@ -0,0 +1,48 @@
+#include "util/EffectBase.h"
+#include "util/GameUtil.h"
+#include "util/GenericUtil.h"
+#include "util/MathHelper.h"
+
+class StruckByTruckEffect : public EffectBase
+{
+    int wait = 700;
+
+public:
+    void
+    OnStart (EffectInstance *inst) override
+    {
+        wait = 700;
+
+        inst->SetTimerVisible (false);
+    }
+
+    void
+    OnTick (EffectInstance *inst) override
+    {
+        wait -= (int) GenericUtil::CalculateTick ();
+        if (wait > 0) return;
+
+        CPlayerPed *player = FindPlayerPed ();
+        if (!player) return;
+
+        CVector position
+            = player->TransformFromObjectSpace (CVector (0.0f, 25.0f, 0.0f));
+
+        // Linerunner
+        CAutomobile *vehicle = (CAutomobile *) GameUtil::CreateVehicle (
+            403, position, player->m_fCurrentRotation - M_PI, true);
+
+        vehicle->PlaceOnRoadProperly ();
+
+        CMatrix *matrix   = vehicle->GetMatrix ();
+        float    velocity = 1.0f;
+
+        vehicle->m_vecMoveSpeed.x = velocity * matrix->up.x;
+        vehicle->m_vecMoveSpeed.y = velocity * matrix->up.y;
+        vehicle->m_vecMoveSpeed.z = velocity * matrix->up.z;
+
+        inst->Disable ();
+    }
+};
+
+DEFINE_EFFECT (StruckByTruckEffect, "effect_struck_by_truck", 0);
