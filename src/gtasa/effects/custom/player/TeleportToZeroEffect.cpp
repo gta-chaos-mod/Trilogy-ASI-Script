11614c6abe66f283d92330dbbdecd8e8f3530fd4
Lordmau5 <mail@lordmau5.com>
2022-06-29 18:06:31 +0200

    Add effect `Teleport To Zero`

---
+++ src/gtasa/effects/custom/player/TeleportToZeroEffect.cpp
@@ -0,0 +1,45 @@
+#include "util/EffectBase.h"
+#include "util/Teleportation.h"
+
+#include <CStreaming.h>
+#include <extensions/ScriptCommands.h>
+
+using namespace plugin;
+
+class TeleportToZeroEffect : public EffectBase
+{
+public:
+    bool
+    CanActivate () override
+    {
+        return Teleportation::CanTeleport ();
+    }
+
+    void
+    OnTick (EffectInstance *inst) override
+    {
+        if (!CanActivate ())
+        {
+            inst->ResetTimer ();
+            return;
+        }
+
+        CPlayerPed *player = FindPlayerPed ();
+        if (!player) return;
+
+        if (player->m_pVehicle)
+        {
+            Command<eScriptCommands::
+                        COMMAND_REMOVE_CHAR_FROM_CAR_MAINTAIN_POSITION> (
+                player, player->m_pVehicle);
+        }
+
+        Teleportation::Teleport (CVector (-2245.66f, 128.89f, 35.0f));
+        player->m_vecMoveSpeed = CVector (0, 0, 0);
+        Command<eScriptCommands::COMMAND_RESTORE_CAMERA_JUMPCUT> ();
+
+        inst->Disable ();
+    }
+};
+
+DEFINE_EFFECT (TeleportToZeroEffect, "effect_teleport_to_zero", 0);
