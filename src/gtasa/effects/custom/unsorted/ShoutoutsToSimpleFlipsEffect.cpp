3afbb6edd42f97ab550e9e4677acd14b7c58e802
Lordmau5 <mail@lordmau5.com>
2022-06-29 18:06:23 +0200

    Update `Shoutouts to SimpleFlips` code

--- src/gtasa/effects/custom/unsorted/ShoutoutsToSimpleFlipsEffect.cpp
+++ src/gtasa/effects/custom/unsorted/ShoutoutsToSimpleFlipsEffect.cpp
@@ -2,6 +2,7 @@
 #include "util/Globals.h"
 #include "util/hooks/HookMacros.h"
 
+#include <CFont.h>
 #include <CText.h>
 
 class ShoutoutsToSimpleFlipsEffect : public EffectBase
@@ -14,6 +15,12 @@ public:
 
         HOOK_METHOD_ARGS (inst, Hooked_CText_Get, char *(CText *, char *),
                           0x6A0050);
+
+        HOOK_ARGS (inst, Hooked_PrintMoney, char (float, float, char *),
+                   0x58F607);
+
+        HOOK_ARGS (inst, Hooked_PrintClockAndAmmo, char (float, float, char *),
+                   0x58EC21, 0x58962A);
     }
 
     void
@@ -26,6 +33,30 @@ public:
     Hooked_CText_Get (auto &&cb, CText *text, char *key)
     {
         return (char *) "Shoutouts to SimpleFlips.";
+    }
+
+    static char
+    Hooked_PrintClockAndAmmo (auto &&cb, float x, float y, char *&text)
+    {
+        text = (char *) "Shoutouts to SimpleFlips.";
+
+        CFont::SetProportional (true);
+        char result = cb ();
+        CFont::SetProportional (false);
+
+        return result;
+    }
+
+    static char
+    Hooked_PrintMoney (auto &&cb, float x, float y, char *&text)
+    {
+        text = (char *) "$Shoutouts to SimpleFlips.";
+
+        CFont::SetProportional (true);
+        char result = cb ();
+        CFont::SetProportional (false);
+
+        return result;
     }
 };
 
