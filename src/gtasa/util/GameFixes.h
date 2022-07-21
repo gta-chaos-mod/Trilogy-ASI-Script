21f5bade0365cb5a90bef02d38c4f430f195b59e
Lordmau5 <mail@lordmau5.com>
2022-07-21 14:45:06 +0200

    Refactor GameFixes
    Also improve "High Stakes, Low-Rider" fade fix for when Cesar's car explodes before getting into the race

--- src/gtasa/util/GameFixes.h
+++ src/gtasa/util/GameFixes.h
@@ -4,6 +4,7 @@
 #include "util/EffectHandler.h"
 #include "util/GameUtil.h"
 #include "util/GenericUtil.h"
+#include "util/GlobalHooksInstance.h"
 
 #include <CText.h>
 
@@ -16,25 +17,36 @@ public:
     Initialise ()
     {
         // Properly modify sound pitch and speed
-        for (int address : {0x4EA03B, 0x4EF446})
-        {
-            patch::RedirectCall (address, Hooked_GetIsSlowMotionActive);
-        }
+        HOOK (GlobalHooksInstance::Get (), Hooked_GetIsSlowMotionActive,
+              bool (), 0x4EA03B, 0x4EF446);
+        // for (int address : {0x4EA03B, 0x4EF446})
+        // {
+        //     patch::RedirectCall (address, Hooked_GetIsSlowMotionActive);
+        // }
         injector::WriteMemory (0x8CBA6C, 1.0f, true);
 
         // Fix Cesar Vialpando fade-out when it shouldn't happen
-        patch::RedirectCall (
-            0x47C7C7,
-            Hooked_CRunningScript_CollectParameters); // OPCODE: 016A, Fade
+        // OPCODE: 016A, Fade
+        HOOK_METHOD_ARGS (GlobalHooksInstance::Get (),
+                          Hooked_CRunningScript_CollectParameters,
+                          __int16 (CRunningScript *, __int16), 0x47C7C7);
+        // patch::RedirectCall (0x47C7C7,
+        // Hooked_CRunningScript_CollectParameters);
 
         // Fix certain cheats cancelling each other out
-        for (int address : {0x43934E, 0x4394C6, 0x4394F6, 0x439856})
-        {
-            patch::RedirectCall (address, Hooked_CheatCancelEachOther);
-        }
+        HOOK (GlobalHooksInstance::Get (), Hooked_CheatCancelEachOther,
+              char (int), 0x43934E, 0x4394C6, 0x4394F6, 0x439856);
+        // for (int address : {0x43934E, 0x4394C6, 0x4394F6, 0x439856})
+        // {
+        //     patch::RedirectCall (address, Hooked_CheatCancelEachOther);
+        // }
 
         // Fix crash on trying to spawn a train as a boat
-        patch::RedirectCall (0x501F3B, Hooked_ProcessTrainTrackSound);
+        HOOK_METHOD_ARGS (GlobalHooksInstance::Get (),
+                          Hooked_ProcessTrainTrackSound,
+                          void (CAEVehicleAudioEntity *, cVehicleParams *),
+                          0x501F3B); // OPCODE: 016A, Fade
+        // patch::RedirectCall (0x501F3B, Hooked_ProcessTrainTrackSound);
 
         // Disable radio volume => -100 if slowmo is active
         patch::Nop (0x4EA044, 8);
@@ -44,24 +56,28 @@ public:
     }
 
     static bool
-    Hooked_GetIsSlowMotionActive ()
+    Hooked_GetIsSlowMotionActive (auto &&cb)
     {
         return true;
     }
 
-    static __int16 __fastcall Hooked_CRunningScript_CollectParameters (
-        CRunningScript *thisScript, void *edx, unsigned __int16 num)
+    static __int16
+    Hooked_CRunningScript_CollectParameters (auto           &&cb,
+                                             CRunningScript  *thisScript,
+                                             unsigned __int16 num)
     {
-        __int16 result
-            = CallMethodAndReturn<__int16, 0x464080, CRunningScript *> (
-                thisScript, num);
+        __int16 result = cb ();
 
         std::string missionName
             = GenericUtil::ToUpper (std::string (thisScript->m_szName));
         if (missionName == "CESAR1")
         {
+            int currentOffset = thisScript->m_pCurrentIP
+                                - (unsigned char *) CTheScripts::ScriptSpace;
+
             if (CTheScripts::ScriptParams[0].iParam == 0
-                && CTheScripts::ScriptParams[1].iParam == 0)
+                && CTheScripts::ScriptParams[1].iParam == 0
+                && currentOffset == 205104)
             {
                 CTheScripts::ScriptParams[1].iParam = 1;
             }
@@ -71,13 +87,14 @@ public:
     }
 
     static char
-    Hooked_CheatCancelEachOther (int id)
+    Hooked_CheatCancelEachOther (auto &&cb)
     {
         return 0;
     }
 
-    static void __fastcall Hooked_ProcessTrainTrackSound (
-        CAEVehicleAudioEntity *thisEntity, void *edx, cVehicleParams *params)
+    static void
+    Hooked_ProcessTrainTrackSound (auto &&cb, CAEVehicleAudioEntity *thisEntity,
+                                   cVehicleParams *params)
     {
         if (params->m_nVehicleSubclass == VEHICLE_BOAT)
         {
