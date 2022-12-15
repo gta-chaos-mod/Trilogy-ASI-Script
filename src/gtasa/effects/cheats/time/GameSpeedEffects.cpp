91056b0c172e846f0ca44578c2ccd55b4d4a351d
Lordmau5 <mail@lordmau5.com>
2022-12-15 14:51:31 +0100

    Add experimental code to `Game Speed` effects Decompiled tBeatInfo and tBeatInfoBeat from the mobile game

--- src/gtasa/effects/cheats/time/GameSpeedEffects.cpp
+++ src/gtasa/effects/cheats/time/GameSpeedEffects.cpp
@@ -5,6 +5,25 @@
 #include <CAEAudioHardware.h>
 #include <CTimer.h>
 
+class tBeatInfoBeat
+{
+public:
+    int Time;
+    int Type;
+};
+VALIDATE_SIZE (tBeatInfoBeat, 0x8);
+
+class tBeatInfo
+{
+public:
+    tBeatInfoBeat BeatWindow[20];
+    int           bBeatInfoPresent;
+    int           BeatTypeThisFrame;
+    int           BeatNumber;
+};
+
+VALIDATE_SIZE (tBeatInfo, 0xAC);
+
 using namespace plugin;
 
 template <float timeScale, float audioPitch>
@@ -17,6 +36,9 @@ public:
         HOOK_METHOD_ARGS (inst, Hooked_SetFrequencyScalingFactor,
                           int (CAEAudioHardware *, int, int, float), 0x4D6E34,
                           0x4D6E48, 0x4DBF9B, 0x4EA62D, 0x4F0871, 0x4F0A58);
+
+        // HOOK_METHOD_ARGS (inst, Hooked_CAEAudioHardware__GetBeatInfo,
+        //                   int (CAEAudioHardware *, tBeatInfo *), 0x5071BA);
     }
 
     void
@@ -44,6 +66,23 @@ public:
         if (factor > 0.0f) factor = audioPitch;
 
         return cb ();
+    }
+
+    static int
+    Hooked_CAEAudioHardware__GetBeatInfo (auto            &&cb,
+                                          CAEAudioHardware *thisAudioHardware,
+                                          tBeatInfo        *beatInfo)
+    {
+        int result = cb ();
+
+        for (int i = 0; i < 20; i++)
+        {
+            beatInfo->BeatWindow[i].Time /= audioPitch;
+        }
+
+        beatInfo->BeatNumber = 10;
+
+        return result;
     }
 };
 
