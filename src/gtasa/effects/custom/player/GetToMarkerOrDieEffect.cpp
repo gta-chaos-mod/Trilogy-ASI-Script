bd6359b978b7e1f56917756b82ede50076cfa162
Lordmau5 <mail@lordmau5.com>
2022-12-16 15:03:57 +0100

    Add new effect `Get To The Marker Or Die`

---
+++ src/gtasa/effects/custom/player/GetToMarkerOrDieEffect.cpp
@@ -0,0 +1,139 @@
+#include "util/EffectBase.h"
+#include "util/GenericUtil.h"
+
+#include <CCheckpoints.h>
+#include <CRadar.h>
+#include <CStreaming.h>
+#include <extensions/ScriptCommands.h>
+
+using namespace plugin;
+
+// TODO: If you don't get to marker you get a penalty.
+// - 5 Mins. of One Hit KO only for CJ?
+
+// DEFINITELY Test for CC
+// Add effect queue for this specific case:
+// About to fail this effect (to get 5 mins of ohko) AND someone just buys
+// Infinite Health. Infinite Health would get disabled, so they would have their
+// money wasted.
+
+// TODO: Increase effect duration based on distance
+
+class GetToMarkerOrDieEffect : public EffectBase
+{
+    int     checkpoint        = 0;
+    int     coordBlip         = 0;
+    CVector position          = {0.0, 0.0f, 0.0f};
+    bool    positionGenerated = false;
+    bool    reachedMarker     = false;
+
+public:
+    bool
+    CanActivate () override
+    {
+        return true;
+    }
+
+    void
+    OnStart (EffectInstance *inst) override
+    {
+        checkpoint        = 0;
+        coordBlip         = 0;
+        positionGenerated = false;
+        reachedMarker     = false;
+    }
+
+    void
+    OnEnd (EffectInstance *inst) override
+    {
+        if (coordBlip != 0)
+        {
+            CRadar::ClearBlip (coordBlip);
+        }
+
+        if (checkpoint != 0)
+        {
+            Command<eScriptCommands::COMMAND_DELETE_CHECKPOINT> (checkpoint);
+        }
+
+        CPlayerPed *player = FindPlayerPed ();
+        if (!player) return;
+
+        if (!reachedMarker)
+        {
+            Command<eScriptCommands::COMMAND_SET_CHAR_HEALTH> (player, 0);
+        }
+    }
+
+    void
+    OnTick (EffectInstance *inst) override
+    {
+        CPlayerPed *player = FindPlayerPed ();
+        if (!player || !CanActivate ()) return;
+
+        if (!positionGenerated)
+        {
+            position = FindSuitableMarkerPosition (inst);
+
+            Command<eScriptCommands::COMMAND_ADD_BLIP_FOR_COORD> (position.x,
+                                                                  position.y,
+                                                                  position.z,
+                                                                  &coordBlip);
+            CRadar::ChangeBlipScale (coordBlip, 3);
+            CRadar::ChangeBlipColour (coordBlip,
+                                      CRGBA (0, 150, 255, 255).ToInt ());
+
+            Command<eScriptCommands::COMMAND_CREATE_CHECKPOINT> (
+                1, position.x, position.y, position.z, position.x, position.y,
+                position.z, 4.0f, &checkpoint);
+
+            positionGenerated = true;
+            return;
+        }
+
+        CRadar::ChangeBlipDisplay (coordBlip,
+                                   GenericUtil::IsMenuActive ()
+                                       ? eBlipDisplay::BLIP_DISPLAY_NEITHER
+                                       : eBlipDisplay::BLIP_DISPLAY_BOTH);
+
+        if (!Command<eScriptCommands::COMMAND_LOCATE_CHAR_ANY_MEANS_2D> (
+                player, position.x, position.y, 4.0f, 4.0f, true))
+            return;
+
+        reachedMarker = true;
+        inst->Disable ();
+    }
+
+    CVector
+    FindSuitableMarkerPosition (EffectInstance *inst)
+    {
+        CVector randomPosition
+            = CVector (inst->Random (-3000.0f, 3000.0f),
+                       inst->Random (-3000.0f, 3000.0f), 0.0f);
+
+        Command<eScriptCommands::COMMAND_REQUEST_COLLISION> (randomPosition.x,
+                                                             randomPosition.y);
+        CStreaming::StreamZoneModels (&randomPosition);
+        CStreaming::LoadAllRequestedModels (false);
+
+        bool groundResult = false;
+        randomPosition.z
+            = CWorld::FindGroundZFor3DCoord (randomPosition.x, randomPosition.y,
+                                             randomPosition.z + 250.0f,
+                                             &groundResult, 0)
+              + 3.0f;
+
+        if (!groundResult) return FindSuitableMarkerPosition (inst);
+
+        float waterLevel = 0.0f;
+        Command<eScriptCommands::COMMAND_GET_WATER_HEIGHT_AT_COORDS> (
+            randomPosition.x, randomPosition.y, true, &waterLevel);
+
+        randomPosition.z = std::max (randomPosition.z, waterLevel);
+
+        return randomPosition;
+    }
+};
+
+DEFINE_EFFECT (GetToMarkerOrDieEffect, "effect_get_to_marker_or_die",
+               GROUP_HEALTH);
