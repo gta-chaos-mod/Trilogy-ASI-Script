5ae22cbce3b5db5b9c8d6bf69eeed7f23e3efc52
Lordmau5 <mail@lordmau5.com>
2022-12-26 01:01:30 +0100

    Refactor config related code

--- src/shared/util/Config.cpp
+++ src/shared/util/Config.cpp
@@ -1,6 +1,6 @@
 #include "Config.h"
 
-#include "util/FileUpdateListener.cpp"
+#include "util/FileUpdateListener.h"
 
 #include <efsw/efsw.hpp>
 
--- src/shared/util/FileUpdateListener.h
+++ src/shared/util/FileUpdateListener.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "util/Config.h"
 #include "util/Websocket.h"
 
