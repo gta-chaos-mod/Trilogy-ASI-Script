974a6ec3ac1b6267c3337525a6680b34cbdd0da7
gromchek <38837437+gromchek@users.noreply.github.com>
2023-01-09 18:36:16 +0300

    New effect: `Respect The Law` #206 (#207)

---
+++ src/gtasa/effects/custom/wanted/RespectTheLawEffect.cpp
@@ -0,0 +1,30 @@
+//Effect created by gromchek
+
+#include "util/EffectBase.h"
+
+class RespectTheLawEffect : public EffectBase
+{
+public:
+    void
+    OnTick (EffectInstance *inst) override
+    {
+        CPlayerPed *player = FindPlayerPed ();
+        if (!player) return;
+
+        CWanted *wanted = FindPlayerWanted (-1);
+        if (!wanted) return;
+
+        if (!player->CanSetPedState () || !player->IsAlive ())
+        {
+            return;
+        }
+
+        if (wanted->m_nWantedLevel > 0)
+        {
+            player->SetPedState (PEDSTATE_ARRESTED);
+        }
+    }
+};
+
+DEFINE_EFFECT (RespectTheLawEffect, "effect_respect_the_law",
+               GROUP_WANTED);
