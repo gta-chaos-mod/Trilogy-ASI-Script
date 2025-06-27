06f8ae7274943cd82694c57628e1c0c5a5cbd7f4
gromchek <38837437+gromchek@users.noreply.github.com>
2025-06-27 20:49:31 +0300

    Make cough effects less annoying (#307)

--- src/gtasa/effects/custom/ped/CoughEffect.cpp
+++ src/gtasa/effects/custom/ped/CoughEffect.cpp
@@ -8,12 +8,11 @@ using namespace plugin;
 class CoughEffect : public EffectBase
 {
 private:
-    int       timer              = 0;
-    const int MIN_COUGH_TIME_SEC = 3;
-    const int MAX_COUGH_TIME_SEC = 4;
-    const int MAX_WAIT_TIME_SEC  = 7;
-    const int MIN_WAIT_TIME_SEC  = 5;
-    bool      isPlayerOnly       = false;
+    int       timer             = 0;
+    const int COUGH_TIME        = 3000;
+    const int MAX_WAIT_TIME_SEC = 8;
+    const int MIN_WAIT_TIME_SEC = 6;
+    bool      isPlayerOnly      = false;
 
 public:
     CoughEffect (bool playerOnly) : isPlayerOnly (playerOnly) {}
@@ -60,27 +59,50 @@ public:
         if (!ped) return;
 
         auto *vehicle = ped->m_pVehicle;
-        if (vehicle && vehicle->m_pDriver == ped
-            && vehicle->m_vecMoveSpeed.Magnitude () > 0.15f)
+
+        if (vehicle)
         {
-            auto speed = vehicle->m_vecMoveSpeed.Magnitude ()
-                         * (inst->Random (0, 1) ? 0.12f : -0.12f);
-            vehicle->m_vecTurnSpeed.z = speed;
+            const int vehicleId   = vehicle->m_nModelIndex;
+            auto      vehicleType = CModelInfo::IsVehicleModelType (vehicleId);
+            bool      canSpin     = false;
+            switch (vehicleType)
+            {
+                case VEHICLE_AUTOMOBILE:
+                case VEHICLE_BIKE: canSpin = true; break;
+                default: break;
+            }
+            canSpin &= vehicle->m_pDriver == ped;
+            canSpin &= vehicle->m_vecMoveSpeed.Magnitude () > 0.15f;
+            canSpin &= inst->Random (0, 100000) % 2 == 0;
+
+            if (canSpin)
+            {
+                auto speed = (inst->Random (0, 1) ? 0.025f : -0.025f);
+                vehicle->m_vecTurnSpeed.z += speed;
+            }
         }
-        if (!ped->m_nPedFlags.bInVehicle)
+
+        bool canPlayAnim = Command<Commands::IS_CHAR_ON_FOOT> (ped);
+        canPlayAnim &= !ped->m_nPedFlags.bInVehicle;
+        canPlayAnim &= !Command<Commands::IS_CHAR_IN_WATER> (ped);
+        if (ped == FindPlayerPed ())
         {
-            int sec
-                = inst->Random (MIN_COUGH_TIME_SEC, MAX_COUGH_TIME_SEC) * 1000;
+            canPlayAnim &= !Command<Commands::IS_PLAYER_USING_JETPACK> (0);
+            canPlayAnim &= GameUtil::IsPlayerSafe ();
+        }
+
+        if (canPlayAnim)
+        {
             Command<eScriptCommands::COMMAND_TASK_PLAY_ANIM_NON_INTERRUPTABLE> (
-                ped, "gas_cwr", "ped", 4.0, 1, 1, 1, 0, sec);
+                ped, "gas_cwr", "ped", 4.0, 1, 1, 1, 0, COUGH_TIME);
         }
-        ped->m_fHealth -= ped->m_fMaxHealth * 0.02f;
+        ped->m_fHealth -= ped->m_fMaxHealth * 0.03f;
         ped->m_fHealth = std::max (0.0f, ped->m_fHealth);
 
         int res        = 0;
         int speechBank = 340;
         Command<eScriptCommands::COMMAND_SET_CHAR_SAY_CONTEXT_IMPORTANT> (
-            ped, speechBank, 1, 1, 1 & res);
+            ped, speechBank, 1, 1, 1, &res);
     }
 };
 
