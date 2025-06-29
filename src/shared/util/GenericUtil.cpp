670ae3d1fcdd9bd214863bb54bcbdc706b6e5fb2
Lordmau5 <mail@lordmau5.com>
2025-06-29 12:18:31 +0200

    revert

--- src/shared/util/GenericUtil.cpp
+++ src/shared/util/GenericUtil.cpp
@@ -8,7 +8,7 @@
 std::string
 GenericUtil::GetModVersion ()
 {
-    std::string version = "Chaos Mod v3.4.2";
+    std::string version = "Chaos Mod v3.3.0";
 
 #ifndef NDEBUG
     version.append ("-debug");
